c804b0eecb4360c009ff19a2d6a4773bf3523795
emmanuel <emmvazquez@gmail.com>
2025-04-11 19:09:35 -0600

    prueba modulo lora reciver

---
+++ lora_receiver_debug.py
@@ -0,0 +1,36 @@
+import time
+from SX127x.LoRa import LoRa
+from SX127x.board_config import BOARD
+from SX127x.constants import *
+
+BOARD.setup()
+
+class LoRaReceiver(LoRa):
+    def __init__(self, verbose=False):
+        super(LoRaReceiver, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0] * 6)
+
+lora = LoRaReceiver(verbose=False)
+
+lora.set_mode(MODE.STDBY)
+lora.set_freq(915.0)
+lora.set_spreading_factor(7)               # SF7
+lora.set_bw(BW.BW125)                       # 125kHz
+lora.set_coding_rate(CODING_RATE.CR4_5)     # CR4/5
+lora.set_preamble(8)
+lora.set_rx_crc(True)
+lora.set_mode(MODE.RXCONT)
+
+print("🎯 Escuchando flags LoRa cada segundo...")
+
+try:
+    while True:
+        flags = lora.get_irq_flags()
+        print(f"Flags IRQ: {flags}")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("\n⛔ Interrumpido por usuario.")
+finally:
+    BOARD.teardown()
